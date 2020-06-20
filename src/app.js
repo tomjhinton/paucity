592f088a13b6d9b85ec3c2dba9b591eb7a69b061
Tom Hinton <tomjhinton@gmail.com>
2020-06-20 23:22:11 +0100

    dont see  the point in p5

--- src/app.js
+++ src/app.js
@@ -1,34 +1,42 @@
 import 'bulma'
 import './style.scss'
 
-import p5 from 'p5'
+import p5 from "p5";
 
 console.log(p5)
 const s = (sk) => {
   let layers = []
-
+  //let polySynth = new p5moduleSound.PolySynth();
   // sk.translate(window.innerWidth/2,window.innerHeight/2);
   sk.setup = () =>{
-    const gfx = sk.createGraphics(window.innerWidth,window.innerHeight)
-    let gfx2
+    sk.createCanvas(window.innerWidth, window.innerHeight,  sk.WEBGL)
+    sk.background(220);
+    sk.rotateY(1);
 
-    sk.createCanvas(window.innerWidth,window.innerHeight)
-    sk.angleMode(sk.DEGREES)
-    sk.imageMode(sk.CENTER)
-    sk.translate(window.innerWidth/2,window.innerHeight/2)
-    sk.background(40)
-    gfx.stroke(100)
-    gfx.strokeWeight(13)
-    gfx.line(0,0,window.innerWidth,0)
-    for(let i=0;i<1000;i++){
-      gfx.point(Math.random()*window.innerWidth, Math.random()*window.innerHeight)
-    }
 
-    gfx2 = {...gfx}
-    sk.image(gfx,0,0)
-    sk.rotate(1)
-    sk.image(gfx2,0,0)
+      sk.sphere(300)
+
+
+ setInterval(function () {
+   if (sk.mouseIsPressed) {
+    sk.fill( "#"+((1<<24)*Math.random()|0).toString(16));
+
+    sk.sphere(200)
+
+  } else {
+
   }
+  sk.fill( "#"+((1<<24)*Math.random()|0).toString(16));
+
+
+  sk.rotateX(1);
+  sk.rotateY(1);
+
+}, 500);
+
+  }
+
 
 }
 const P5 = new p5(s)
+console.log(P5)
